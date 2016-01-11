1b219903a963bf11c62205467e1576631c60ad13
CKalnasy <kalnasy.6@osu.edu>
2016-01-11 14:22:03 -0500

    using zumba serializer instead

--- Api.php
+++ Api.php
@@ -1,28 +1,33 @@
 <?php
 
+require_once 'vendor/autoload.php';
+use Zumba\Util\JsonSerializer;
+
 /**
  *
  */
 class Api {
-  private static $classes;
+  private $classes;
+  private $serializer;
 
   /**
    *
    */
-  public static function staticInit() {
+  public function __construct() {
     $jsonFile = 'api.json';
     if (file_exists($jsonFile)) {
       $string = file_get_contents($jsonFile);
       $json = json_decode($string, true);
-      self::$classes = $json['Classes'];
+      $this->classes = $json['Classes'];
     }
+    $this->serializer = new JsonSerializer();
   }
 
   /**
    *
    */
-  public static function handleRequest() {
-    self::callFunction(self::getParams());
+  public function handleRequest() {
+    $this->callFunction(self::getParams());
   }
 
   private static function getParams() {
@@ -40,13 +45,12 @@ class Api {
     return $ret;
   }
 
-  private static function callFunction($params) {
+  private function callFunction($params) {
     self::includeClasses();
     $args = [];
     foreach ($params['args'] as $className => $json) {
-      array_push($args, self::jsonToObject($className, $json));
+      array_push($args, $this->serializer->serialize($json));
     }
-
     call_user_func_array([$params['className'], $params['functionName']], $args);
   }
 
@@ -55,83 +59,6 @@ class Api {
       include_once $classInfo['location'];
     }
   }
-
-  private static function jsonToObject($className, $json) {
-    $fullClassName = self::getFullClassNameFromPartialClassName($className);
-    if (!self::hasClass($fullClassName)) {
-      die('could not find class: ' . $fullClassName . '. If a namespace for class is used, set the class name (including the namespace) in the api.json file in the document root');
-    }
-    $class = new ReflectionClass($fullClassName);
-    $constructor = $class->getConstructor();
-    $paramsList = self::getParamsListFromParameters($constructor->getParameters());
-    $values = self::sortParamValues($json, $paramsList);
-
-    return self::initClass($fullClassName, $values);
-  }
-
-  private static function getFullClassNameFromPartialClassName($className) {
-    return isset($classes[$className]) ? $json[$className]['namespace'] : $className;
-  }
-
-  private static function hasClass($className) {
-    return in_array($className, get_declared_classes());
-  }
-
-  private static function getParamsListFromParameters($parameters) {
-    $params = [];
-    foreach ($parameters as $param) {
-      array_push($params, $param->getName());
-    }
-    return $params;
-  }
-
-  private static function sortParamValues($params, $paramsList) {
-    $ret = [];
-    foreach ($paramsList as $param) {
-      if (isset($params[$param])) {
-        if (gettype($params[$param]) == 'array') {
-          $className = key($params[$param]);
-          array_push($ret, self::jsonToObject($className, $params[$param][$className]));
-        } else {
-          array_push($ret, $params[$param]);
-        }
-      } else {
-        array_push(null);
-      }
-    }
-    return $ret;
-  }
-
-  private static function initClass($class, $args) {
-    $reflect  = new ReflectionClass($class);
-    return $reflect->newInstanceArgs($args);
-  }
-
-  public static function serializeObject($obj) {
-    $json = [];
-    $reflectionClass = new ReflectionClass($obj);
-    foreach ($reflectionClass->getProperties(ReflectionProperty::IS_PUBLIC) as $property) {
-      $name = $property->getName();
-      $value = $property->getValue($obj);
-      if (gettype($value) == 'object') {
-        $json[$name] = self::serialize($value);
-      } else {
-        $json[$name] = $value;
-      }
-    }
-    return [$reflectionClass->getName() => $json];
-  }
-
-  public static function serializeObjectArray($array) {
-    $ret = [];
-    foreach ($array as $obj) {
-      array_push($ret, self::serializeObject($obj));
-    }
-    return $ret;
-  }
 }
-
-Api::staticInit();
-Api::handle_request();
 
 ?>
